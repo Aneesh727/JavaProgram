0b5bfc2bb114dea69f024596a6950f2cc742e9e3
Aneesh727 <aneesh.garg2001@gmail.com>
2024-04-08 16:13:16 +0530

    some question of patterns

---
+++ src/LeftTriangleNumbers.java
@@ -0,0 +1,11 @@
+public class LeftTriangleNumbers {
+    public static void main(String[] args) {
+        int row=4;
+        for(int i=4;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
