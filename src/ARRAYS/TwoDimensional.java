a27f6062a55c24285e291658467de822fd3a3aed
Aneesh727 <aneesh.garg2001@gmail.com>
2024-04-10 18:14:32 +0530

    More pattern questions

--- src/ARRAYS/TwoDimensional.java
+++ src/ARRAYS/TwoDimensional.java
@@ -2,17 +2,19 @@ package ARRAYS;
 
 public class TwoDimensional {
     public static void main(String[] args) {
-        int a[][] = new int[3][2];   // declartion
+//        int a[][] = new int[3][2];   // declartion
+
+        int a[][] = {{10,20}, {30,40}, {50,60}};
 
         // storing elements/values in array
-        a[0][0] = 10;
-        a[0][1] = 20;
-
-        a[1][0] = 30;
-        a[1][1] = 40;
-
-        a[2][0] = 50;
-        a[2][1] = 60;
+//        a[0][0] = 10;
+//        a[0][1] = 20;
+//
+//        a[1][0] = 30;
+//        a[1][1] = 40;
+//
+//        a[2][0] = 50;
+//        a[2][1] = 60;
 
         // classic for loop
 //        for(int i=0; i<=2; i++){
